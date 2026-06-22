120f20227fce2a660121c33e1c116232deb2900b
MZ303071 <mzgamerage@gmail.com>
2026-06-22 17:17:20 -0400

    End of Section 2

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users {get;set;}
+    
+}      
